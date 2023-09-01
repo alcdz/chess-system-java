09b811a7f144c5d0f6b422e8c895b5653f58b566
Luis Paranhos <alcdz45@gmail.com>
2023-09-01 16:58:49 -0300

    Pawn

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -26,7 +26,7 @@ public class Rook extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
             p.setRow(p.getRow() - 1);
         }
-        if (getBoard().positionExists(p) && isThereOppenentPiece(p)) {
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
             p.setRow(p.getRow());
         }
@@ -36,7 +36,7 @@ public class Rook extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
             p.setColumn(p.getColumn() - 1);
         }
-        if (getBoard().positionExists(p) && isThereOppenentPiece(p)) {
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
             p.setRow(p.getColumn());
         }
@@ -46,7 +46,7 @@ public class Rook extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
             p.setColumn(p.getColumn() + 1);
         }
-        if (getBoard().positionExists(p) && isThereOppenentPiece(p)) {
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
             p.setRow(p.getColumn());
         }
@@ -56,7 +56,7 @@ public class Rook extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
             p.setRow(p.getRow() + 1);
         }
-        if (getBoard().positionExists(p) && isThereOppenentPiece(p)) {
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
             p.setRow(p.getRow());
         }
